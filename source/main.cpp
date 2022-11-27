15ce86b17c22afe8fb41576f67a877e283a69f21
PriyanshuSharma1 <psneb420@gmail.com>
2022-11-27 20:40:43 +0545

    source files included

---
+++ source/main.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<random>
+#include<algorithm>
+using namespace std;
+#define n 100
+void quicksort(int high, int low)
+{
+
+}
+int main()
+{
+    vector<int> array;
+    for (int i = 0; i < n; i++)
+    {
+    array.push_back(i);
+    }
+    shuffle(array.begin(),array.end(),default_random_engine(1000));
+    {
+        
+    };
+    
+    
+}
